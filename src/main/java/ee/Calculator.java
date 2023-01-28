514f1ea6d349318159ae07128958f74159eb24c6
lavrlav <113281347+lavrlav@users.noreply.github.com>
2023-01-28 10:52:31 +0300

    1.3

--- src/main/java/ee/Calculator.java
+++ src/main/java/ee/Calculator.java
@@ -12,46 +12,32 @@ public class Calculator extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter printWriter = response.getWriter();
-
         int num1 = Integer.parseInt(request.getParameter("number1"));
         int num2 = Integer.parseInt(request.getParameter("number2"));
-        String[] actions = request.getParameterValues("action");
-        String result = request.getParameter("result");
-
-
-        try {
-            printWriter.println("<p> Number : " + num1 + "</p>");
-            printWriter.println("<p> Number : " + num2 + "</p>");
-            for (String action : actions) {
-                switch (action) {
-                    case "+":
-                        System.out.println(num1 + num2);
-
-                        break;
-                    case "-":
-                        System.out.println(num1 - num2);
-
-                        break;
-                    case "*":
-                        System.out.println(num1 * num2);
-                        break;
-                    case "/":
-                        System.out.println(num1 / num2);
-                        break;
-                    default:
-                        System.out.println("Mistake");
-
-
-
-                }printWriter.println("<p> Action: " + action + "</p>");
-
-
-
+        int result;
+        PrintWriter writer = response.getWriter();
+            switch (request.getParameter("act")) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    result = -1;
             }
-            printWriter.println("<p> Result : " + result + "</p>");
-        } finally {
-            printWriter.close();
+            writer.print("<p>" + result + "</p>");
+            writer.close();
+
         }
     }
-}
+
+
+
+
